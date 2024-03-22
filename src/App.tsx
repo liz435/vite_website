eccce584b392eb98925276571f2f3f6a4a342054
Zelong Li <87303807+liz435@users.noreply.github.com>
2024-03-22 01:20:53 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,120 @@
+import * as THREE from 'three'
+import  React, { useRef, useReducer, useMemo } from 'react';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import { OrbitControls, Text, Environment, Lightformer } from '@react-three/drei';
+import { BallCollider, Physics, RigidBody } from '@react-three/rapier';
+import { easing } from 'maath';
+import { Effects } from './Effects.tsx';
+
+const accents = ['#ff4060', '#ffcc00', '#20ffa0', '#4060ff'];
+const shuffle = (accent = 0) => [
+  { color: '#444', roughness: 0.1, metalness: 0.5 },
+  { color: '#444', roughness: 0.1, metalness: 0.5 },
+  { color: '#444', roughness: 0.1, metalness: 0.5 },
+  { color: 'white', roughness: 0.1, metalness: 0.1 },
+  { color: 'white', roughness: 0.1, metalness: 0.1 },
+  { color: 'white', roughness: 0.1, metalness: 0.1 },
+  { color: accents[accent], roughness: 0.1, accent: true },
+  { color: accents[accent], roughness: 0.1, accent: true },
+  { color: accents[accent], roughness: 0.1, accent: true },
+  { color: '#444', roughness: 0.1 },
+  { color: '#444', roughness: 0.3 },
+  { color: '#444', roughness: 0.3 },
+  { color: 'white', roughness: 0.1 },
+  { color: 'white', roughness: 0.2 },
+  { color: 'white', roughness: 0.1 },
+  { color: accents[accent], roughness: 0.1, accent: true, transparent: true, opacity: 0.5 },
+  { color: accents[accent], roughness: 0.3, accent: true },
+  { color: accents[accent], roughness: 0.1, accent: true }
+];
+
+export default function App(props) {
+  const [accent, click] = useReducer((state) => ++state % accents.length, 0);
+  const connectors = useMemo(() => shuffle(accent), [accent]);
+
+  return (
+    <>
+      <div style={{ width: "100vw", height: "100vh", position: "relative", zIndex: "0" }}>
+        <Canvas flat shadows onClick={click} dpr={[1, 1.5]} gl={{ antialias: false }} camera={{ position: [0, 0, 30], fov: 17.5, near: 10, far: 40 }} {...props}>
+          <color attach="background" args={['#141622']} />
+          <OrbitControls />
+          <TextMesh />
+          <Physics /*debug*/ timeStep="vary" gravity={[0, 1, 2]}>
+            <Pointer />
+            {connectors.map((props, i) => (
+              <Sphere key={i} {...props} />
+            ))}
+          </Physics>
+          <Environment resolution={256}>
+            <group rotation={[-Math.PI / 3, 0, 1]}>
+              <Lightformer form="circle" intensity={100} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={2} />
+              <Lightformer form="circle" intensity={2} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={2} />
+              <Lightformer form="circle" intensity={2} rotation-y={Math.PI / 2} position={[-5, -1, -1]} scale={2} />
+              <Lightformer form="circle" intensity={2} rotation-y={-Math.PI / 2} position={[10, 1, 0]} scale={8} />
+              <Lightformer form="ring" color="#4060ff" intensity={80} onUpdate={(self) => self.lookAt(0, 0, 0)} position={[10, 10, 0]} scale={10} />
+            </group>
+          </Environment>
+          <Effects />
+        </Canvas>
+
+        <div style={{ position: 'absolute', top: 20, left: 20, zIndex: 10,  }}>
+          <ul>
+          <li><button style={{ marginRight: '10px', marginBottom:'10px',marginTop: '60vh' }} onClick={() => alert('Button 1 clicked')}>This Button Does Nothing</button></li>
+          <li><button onClick={() => alert('Button 2 clicked')}>This Button Does Nothing 2</button></li>
+          </ul>
+        </div>
+      </div>
+      <div style={{ position: "absolute", zIndex: "1" }}>
+      </div>
+    </>
+  );
+}
+
+function TextMesh() {
+  const { camera } = useThree();
+  const groupRef = useRef();
+
+  useFrame(() => {
+    groupRef.current.position.copy(camera.position);
+    groupRef.current.rotation.copy(camera.rotation);
+  });
+
+  return (
+    <group ref={groupRef}>
+      <Text color="white" anchorX="center" anchorY="middle" fontSize={2} maxWidth={200}>
+        Hello, React Three Fiber!
+      </Text>
+    </group>
+  );
+}
+
+function Sphere({ position, children, vec = new THREE.Vector3(), scale, r = THREE.MathUtils.randFloatSpread, accent, color = 'white', ...props }) {
+  const api = useRef()
+  const ref = useRef()
+  const pos = useMemo(() => position || [r(10), r(10), r(10)], [])
+  useFrame((state, delta) => {
+    delta = Math.min(0.1, delta)
+    api.current?.applyImpulse(vec.copy(api.current.translation()).negate().multiplyScalar(0.2))
+    easing.dampC(ref.current.material.color, color, 0.2, delta)
+  })
+  return (
+    <RigidBody linearDamping={4} angularDamping={1} friction={0.1} position={pos} ref={api} colliders={false}>
+      <BallCollider args={[1]} />
+      <mesh ref={ref} castShadow receiveShadow>
+        <sphereGeometry args={[1, 64, 64]} />
+        <meshStandardMaterial {...props} />
+        {children}
+      </mesh>
+    </RigidBody>
+  )
+}
+
+function Pointer({ vec = new THREE.Vector3() }) {
+  const ref = useRef()
+  useFrame(({ mouse, viewport }) => ref.current?.setNextKinematicTranslation(vec.set((mouse.x * viewport.width) / 2, (mouse.y * viewport.height) / 2, 0)))
+  return (
+    <RigidBody position={[0, 0, 0]} type="kinematicPosition" colliders={false} ref={ref}>
+      <BallCollider args={[1]} />
+    </RigidBody>
+  );
+}
